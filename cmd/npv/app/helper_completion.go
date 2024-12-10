c25a5f8874904b7c4ba62583dc7e36f74462595b
Daichi Sakaue <daichi-sakaue@cybozu.co.jp>
2024-12-10 16:12:18 +0900

    Complete --from and --to fields (#20)
    * Complete --from and --to fields
    * Move proxy-related helper functions to a dedicated file
    
    Signed-off-by: Daichi Sakaue <daichi-sakaue@cybozu.co.jp>

--- cmd/npv/app/helper_completion.go
+++ cmd/npv/app/helper_completion.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"context"
+	"strings"
 
 	"github.com/spf13/cobra"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -45,4 +46,39 @@ func completePods(cmd *cobra.Command, args []string, toComplete string) (ret []s
 		ret = append(ret, p.Name)
 	}
 	return
+}
+
+func completeNamespacePods(cmd *cobra.Command, args []string, toComplete string) (ret []string, directive cobra.ShellCompDirective) {
+	ret = make([]string, 0)
+	directive = cobra.ShellCompDirectiveNoFileComp
+
+	clientset, _, err := createK8sClients()
+	if err != nil {
+		return
+	}
+
+	li := strings.Split(toComplete, "/")
+	switch len(li) {
+	case 2: // namespace already filled
+		pods, err := listRelevantPods(context.Background(), clientset, li[0])
+		if err != nil {
+			return
+		}
+		for _, p := range pods {
+			ret = append(ret, li[0]+"/"+p.Name)
+		}
+		return
+
+	default:
+		nss, err := clientset.CoreV1().Namespaces().List(context.Background(), metav1.ListOptions{})
+		if err != nil {
+			return
+		}
+
+		for _, ns := range nss.Items {
+			ret = append(ret, ns.Name+"/")
+		}
+		directive = cobra.ShellCompDirectiveNoSpace
+		return
+	}
 }
