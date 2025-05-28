b67b3f786fd710e64dae575bf487da0b0ed1c9a1
Hiroki Hanada <hiroki-hanada@cybozu.co.jp>
2025-05-28 16:42:19 +0900

    set default proxy namespace to cilium-agent-proxy
    Signed-off-by: Hiroki Hanada <hiroki-hanada@cybozu.co.jp>

--- cmd/npv/app/root.go
+++ cmd/npv/app/root.go
@@ -25,7 +25,7 @@ func init() {
 	rootCmd.PersistentFlags().StringVarP(&rootOptions.namespace, "namespace", "n", "default", "namespace of a pod")
 	rootCmd.PersistentFlags().StringVar(&rootOptions.proxySelector, "proxy-selector", "app.kubernetes.io/name=cilium-agent-proxy", "label selector to find the proxy pods")
 	rootCmd.PersistentFlags().Uint16Var(&rootOptions.proxyPort, "proxy-port", 8080, "port number of the proxy endpoints")
-	rootCmd.PersistentFlags().StringVar(&rootOptions.proxyNamespace, "proxy-namespace", "kube-system", "namespace of the proxy pods")
+	rootCmd.PersistentFlags().StringVar(&rootOptions.proxyNamespace, "proxy-namespace", "cilium-agent-proxy", "namespace of the proxy pods")
 	rootCmd.PersistentFlags().StringVarP(&rootOptions.output, "output", "o", OutputSimple, "output format")
 	rootCmd.PersistentFlags().BoolVar(&rootOptions.noHeaders, "no-headers", false, "stop printing header")
 	rootCmd.RegisterFlagCompletionFunc("namespace", completeNamespaces)
