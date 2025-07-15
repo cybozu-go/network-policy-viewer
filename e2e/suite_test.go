594acf2a5b41d223ea0fc0b728cc561bac292651
Daichi Sakaue <daichi-sakaue@cybozu.co.jp>
2025-07-15 17:43:37 +0900

    Implement basic filter
    Signed-off-by: Daichi Sakaue <daichi-sakaue@cybozu.co.jp>

--- e2e/suite_test.go
+++ e2e/suite_test.go
@@ -1,6 +1,8 @@
 package e2e
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 	"time"
 
@@ -16,11 +18,29 @@ func Test(t *testing.T) {
 var _ = BeforeSuite(func() {
 	SetDefaultEventuallyPollingInterval(time.Second)
 	SetDefaultEventuallyTimeout(5 * time.Minute)
+
+	processTestTraffic()
 })
 
 var _ = Describe("Test network-policy-viewer", func() {
 	runTest()
 })
+
+func processTestTraffic() {
+	data := kubectlSafe(Default, nil, "get", "pod", "-n=test", "-l=test=self", "-o=jsonpath={.items[*].metadata.name}")
+	selfNames := strings.Fields(string(data))
+	l3PodName := onePodByLabelSelector(Default, "test", "test=l3-ingress-explicit-allow-all")
+	l4PodName := onePodByLabelSelector(Default, "test", "test=l4-ingress-explicit-allow-tcp")
+	l3PodIP := string(kubectlSafe(Default, nil, "get", "pod", "-n=test", l3PodName, "-o=jsonpath={.status.podIP}"))
+	l4PodIP := string(kubectlSafe(Default, nil, "get", "pod", "-n=test", l4PodName, "-o=jsonpath={.status.podIP}"))
+
+	kubectlSafe(Default, nil, "exec", "-n=test", selfNames[0], "--", "dig", "@1.1.1.1", "google.com")
+	kubectlSafe(Default, nil, "exec", "-n=test", selfNames[1], "--", "dig", "@8.8.8.8", "google.com")
+	for _, p := range selfNames {
+		kubectlSafe(Default, nil, "exec", "-n=test", p, "--", "curl", fmt.Sprintf("http://%s:8000", l3PodIP))
+		kubectlSafe(Default, nil, "exec", "-n=test", p, "--", "curl", fmt.Sprintf("http://%s:8000", l4PodIP))
+	}
+}
 
 func runTest() {
 	Context("dump", testDump)
